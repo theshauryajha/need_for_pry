122a245f7531ab291d810dbd27b56bda19378fd0
Shreyas <shrs.jha@gmail.com>
2024-09-08 16:57:02 -0700

    update args for start up

--- launch/launch_tmux.sh
+++ launch/launch_tmux.sh
@@ -1,24 +1,42 @@
 #!/bin/bash
 
+# Default values
+USE_JOY=true
+
+# Parse command-line arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --keyboard) USE_JOY=false ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
 # Start a new tmux session named 'drone_race' and detach
-tmux new-session -s drone_race -d
+tmux new-session -s drone_race -d || { echo "Failed to start tmux session"; exit 1; }
 
 # Create and configure the first window for roslaunch
-tmux rename-window -t drone_race:0 'roslaunch'
-tmux send-keys -t drone_race:0 'source ~/hector_quadrotor_ws/devel/setup.bash' C-m
-tmux send-keys -t drone_race:0 'roslaunch drone_race drone_race.launch' C-m
+tmux rename-window -t drone_race:0 'roslaunch' || { echo "Failed to rename tmux window"; exit 1; }
+tmux send-keys -t drone_race:0 'source ~/hector_quadrotor_ws/devel/setup.bash' C-m 
+tmux send-keys -t drone_race:0 "roslaunch drone_race drone_race.launch use_joy:=${USE_JOY}" C-m || { echo "Failed to send command to tmux window"; exit 1; }
 
-# Split the 'roslaunch' window horizontally
-tmux split-window -h -t drone_race:0
+# Create and configure the second window with teleop_twist_keyboard if required
+if [ "$USE_JOY" = false ]; then
+    # Wait for roslaunch to complete
+    sleep 2  
 
-sleep 2
+    # Split the 'roslaunch' window horizontally
+    tmux split-window -h -t drone_race:0 || { echo "Failed to split tmux window"; exit 1; }
+    tmux send-keys -t drone_race:0 'clear' C-m  # Clear the split window
 
-# Create and configure the second window for joy_to_twist status
-tmux new-window -t drone_race:1 -n 'joy_status'
-tmux send-keys -t drone_race:1 'rostopic echo /cmd_vel' C-m
-
-# Select the 'joy_status' window to make it active
-tmux select-window -t drone_race:1
+    # Create a new window for teleop_twist_keyboard
+    tmux new-window -t drone_race:1 -n 'teleop_twist_keyboard' || { echo "Failed to create new tmux window"; exit 1; }
+    tmux send-keys -t drone_race:1 'source ~/hector_quadrotor_ws/devel/setup.bash' C-m || { echo "Failed to send command to tmux window"; exit 1; }
+    tmux send-keys -t drone_race:1 'rosrun teleop_twist_keyboard teleop_twist_keyboard.py' C-m || { echo "Failed to send command to tmux window"; exit 1; }
+    
+    # Select the appropriate window to make it active
+    tmux select-window -t drone_race:1
+fi
 
 # Attach to the tmux session
 tmux attach-session -t drone_race
